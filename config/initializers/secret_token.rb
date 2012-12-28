05e811d3858d75a94e701ba497791cf68cd7e8a4
elico <elico_yqf@sina.com>
2012-12-28 12:38:21 +0800

    初始化项目。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logbook::Application.config.secret_token = 'eda2ac7f3d4547a56ad8e817ad7656e07c037b0cf9eb1efad984bc8e1b4f1194ab30541918a776d6dab8cb8f48b17f09dd52b3746691d3e22f98174f51c3f7bc'
